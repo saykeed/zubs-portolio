2ac68f98fcbae280ab4af5dbab2b1f5823bd8c3e
Abubakri Sherif <saykeed@gmail.com>
2022-07-09 17:31:04 +0100

    got rid of aos

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,18 @@
 
 <script setup lang="ts">
   import { onMounted } from "vue";
-  import AOS from "aos";
-  import Navbar from './components/Navbar.vue'
+  // import AOS from "aos";
+  import Navbar from './components/Navbar.vue';
   
 
-  onMounted(() => {
-      AOS.init({
-        duration: 700,
-        easing: 'ease', 
-        once: false,
-        mirror: true,
-      });
-  })
+  // onMounted(() => {
+  //     AOS.init({
+  //       duration: 700,
+  //       easing: 'ease', 
+  //       once: false,
+  //       mirror: true,
+  //     });
+  // })
 </script>
 
 <style lang="scss">
